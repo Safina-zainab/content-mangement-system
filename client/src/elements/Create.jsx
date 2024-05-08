7eb2cd971dd8e4d4f9d32ed3fea04d08f6aff503
safina zainab <safina.cumin@gmail.com>
2024-05-08 20:24:24 +0530

    Create Students Page

--- client/src/elements/Create.jsx
+++ client/src/elements/Create.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Create = () => {
-  return (
-    <div>Create</div>
-  )
+function Create() {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        mobile: '',
+        dob: ''
+    });
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/add_user', values)
+            .then((res) => {
+                navigate('/');
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <div className='container' style={{ maxWidth: '600px' }}>
+            <div className='border rounded shadow p-4 mt-5'>
+                <div className='row'>
+                    <h3>Add Student</h3>
+                    <div className='d-flex justify-content-end'>
+                        <Link to='/' className='btn btn-success'>Home</Link>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className='form-group my-3'>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' name='name' required onChange={(e) => setValues({ ...values, name: e.target.value })} className='form-control' />
+                        </div>
+                        <div className='form-group my-3'>
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' name='email' required onChange={(e) => setValues({ ...values, email: e.target.value })} className='form-control' />
+                        </div>
+                        <div className='form-group my-3'>
+                            <label htmlFor='mobile'>Mobile Number</label>
+                            <input type='tel' name='mobile' required onChange={(e) => setValues({ ...values, mobile: e.target.value })} className='form-control' />
+                        </div>
+                        <div className='form-group my-3'>
+                            <label htmlFor='dob'>Date of Birth</label>
+                            <input type='date' name='dob' required onChange={(e) => setValues({ ...values, dob: e.target.value })} className='form-control' />
+                        </div>
+                        <div className='form-group my-3'>
+                            <button type='submit' className='btn btn-success'>Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Create
+export default Create;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
-const path = require('path')
-
-
-const app = express()
-
-app.use(express.static(path.join(__dirname, "public")))
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+//path.resolve()
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
 
 const port = 5000;
+
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,6 +17,64 @@ const db = mysql.createConnection({
   database: "students",
 });
 
-app.listen(port, () => {
-    console.log(`listening on port ${port} `);
+
+app.post("/add_user", (req, res) => {
+  const sql =
+  " INSERT INTO `student_details` (`id`, `name`, `email`, `mobile`, `dob`) VALUES ('', '', '', '', '')";
+  const values = [req.body.name, req.body.email, req.body.mobile, req.body.dob];
+  db.query(sql, values, (err, result) => {
+    if (err)
+      return res.json({ message: "Something unexpected has occured" + err });
+    return res.json({ success: "Student added successfully" });
   });
+});
+
+// app.get("/students", (req, res) => {
+//   const sql = "SELECT * FROM student_details";
+//   db.query(sql, (err, result) => {
+//     if (err) res.json({ message: "Server error" });
+//     return res.json(result);
+//   });
+// });
+
+// app.get("/get_student/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = "SELECT * FROM student_details WHERE `id`= ?";
+//   db.query(sql, [id], (err, result) => {
+//     if (err) res.json({ message: "Server error" });
+//     return res.json(result);
+//   });
+// });
+
+// app.post("/edit_user/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql =
+//     "UPDATE student_details SET `name`=?, `email`=?, `age`=?, `gender`=? WHERE id=?";
+//   const values = [
+//     req.body.name,
+//     req.body.email,
+//     req.body.age,
+//     req.body.gender,
+//     id,
+//   ];
+//   db.query(sql, values, (err, result) => {
+//     if (err)
+//       return res.json({ message: "Something unexpected has occured" + err });
+//     return res.json({ success: "Student updated successfully" });
+//   });
+// });
+
+// app.delete("/delete/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = "DELETE FROM student_details WHERE id=?";
+//   const values = [id];
+//   db.query(sql, values, (err, result) => {
+//     if (err)
+//       return res.json({ message: "Something unexpected has occured" + err });
+//     return res.json({ success: "Student updated successfully" });
+//   });
+// });
+
+app.listen(port, () => {
+  console.log(`listening on port ${port} `);
+});
