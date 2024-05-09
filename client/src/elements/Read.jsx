fb61ea8b9a7393f8c3fe01b289fa89f6f1b96eb0
safina zainab <safina.cumin@gmail.com>
2024-05-09 07:46:32 +0530

    Read page UI Implemented

--- client/src/elements/Read.jsx
+++ client/src/elements/Read.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
-const Read = () => {
+function Read() {
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/get_student/${id}`)
+      .then((res) => {
+        const formattedData = res.data.map((student) => {
+          const date = new Date(student.dob);
+          const formattedDate = `${date.getDate()}-${months[date.getMonth()]}-${date.getFullYear()}`;
+          return { ...student, dob: formattedDate };
+        });
+        setData(formattedData);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
   return (
-    <div>Read</div>
-  )
+    <div className="container" style={{ maxWidth: '600px', marginTop: '20px' }}>
+      <div className="border rounded shadow p-4">
+        <h1> Student Details
+        <Link to="/" className="btn btn-success" style={{marginLeft: '33%' }}>Back</Link>
+        </h1>
+        {data.map((student) => (
+          <ul key={student.id} className="list-group">
+            <li className="list-group-item">
+              <b>ID: </b>
+              {student.id}
+            </li>
+            <li className="list-group-item">
+              <b>Name: </b>
+              {student.name}
+            </li>
+            <li className="list-group-item">
+              <b>Email: </b>
+              {student.email}
+            </li>
+            <li className="list-group-item">
+              <b>Mobile: </b>
+              {student.mobile}
+            </li>
+            <li className="list-group-item">
+              <b>Date of Birth: </b>
+              {student.dob}
+            </li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Read
+export default Read;
